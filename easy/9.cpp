dda5f09ef6560e14fd1e9b7c377179f2aaf31312
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-07-30 19:12:17 +0545

    feat: solved (easy) qn 9

---
+++ easy/9.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Number{
+    int n;
+public:
+    Number(int a){
+        n = a;
+    }
+
+    int sumFirstLast(){
+        int last = n % 10;
+        int first = n;
+        while(first >= 10){
+            first /= 10;
+        }
+        return first + last;
+    }
+};
+
+int main(){
+    Number n(12345);
+    cout << n.sumFirstLast();
+    return 0;
+}
